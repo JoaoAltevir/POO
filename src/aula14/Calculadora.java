74b263e58f6a57340beec174b454b11463ffedc1
João Neto <j.altevirneto@gmail.com>
2024-12-10 19:48:58 -0300

    add aula 14

---
+++ src/aula14/Calculadora.java
@@ -0,0 +1,15 @@
+package aula14;
+
+public class Calculadora {
+	
+	public int somar(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public double somar(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+	public List<integer> somar(int listaNumeros) 
+	
+}
